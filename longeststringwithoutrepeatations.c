29f63715fbbf1f3b4a0eac3436ae22b68b8194ea
manoharreddyvoladri <92422839+manoharreddyvoladri@users.noreply.github.com>
2023-02-02 11:53:13 +0530

    Create longeststringwithoutrepeatations.c

---
+++ longeststringwithoutrepeatations.c
@@ -0,0 +1,37 @@
+
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+ 
+
+Example :
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+*/
+int lengthOfLongestSubstring(char * s){
+    /*letter_map is to keep track if we saw this character in this substring*/
+    int letter_map[128] = {0}, res = 0;
+    char* start = s, *end = s;
+    
+    while (*end) {
+        /* If we reached a letter we saw already - check max length and start a new substring*/
+        if (letter_map[*end]) {
+            res = (end - start > res) ? end - start : res;
+            while (*start != *end) {
+                letter_map[*start] = 0;
+                start++;
+            }
+            start++;
+            end++;
+        }
+        else {
+            letter_map[*end] = 1;
+            end++;
+        }  
+    }
+    return end-start > res ? end-start : res;
+}
+
