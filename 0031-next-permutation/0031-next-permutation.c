bc3fe5e5c0887f7c79cdb75b8cb70f4c4f0fcb13
manoharreddyvoladri <92422839+manoharreddyvoladri@users.noreply.github.com>
2023-02-05 11:34:30 +0530

    Time: 18 ms (96.98%), Space: 13.5 MB (20.24%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.c
@@ -0,0 +1,17 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        found = False
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i] > nums[i-1]:
+                nums[i:] = nums[i:][::-1]
+                for j in range(i, len(nums)):
+                    if nums[j] > nums[i-1]:
+                        nums[i-1], nums[j] = nums[j], nums[i-1]
+                        break
+                found = True
+                break
+        if not found: nums.reverse()
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
