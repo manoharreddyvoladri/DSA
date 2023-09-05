734141819b42ed2a6349344a2782d591779943f1
manoharreddyvoladri <92422839+manoharreddyvoladri@users.noreply.github.com>
2023-09-05 10:11:20 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (9.53%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,25 @@
+class Solution {
+  public Node copyRandomList(Node head) {
+    if (head == null) return null;
+
+    var len = 0;
+    for (var ptr = head; ptr != null; ptr = ptr.next) len++;
+
+    var newList = new Node[len];
+    var ptr = head;
+
+    for (var i=0; i<len; i++, ptr = ptr.next) {
+      newList[i] = new Node(ptr.val);
+      ptr.val = i;
+    }
+    ptr = head;
+
+    for (var i=0; i<len; i++, ptr = ptr.next) {
+      if (i < len-1)
+        newList[i].next = newList[i+1];
+        
+      newList[i].random = ptr.random == null ? null : newList[ptr.random.val];
+    }
+    return newList[0];
+  }
+}
