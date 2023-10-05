713c0dd8ad287ac24e641e45b5e229c994eda1b4
Manohar Reddy Voladri <92422839+manoharreddyvoladri@users.noreply.github.com>
2023-10-05 20:50:49 +0530

    Time: 3 ms (68.13%), Space: 47.2 MB (10.72%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,26 +1,29 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        if (nums.length < 60) return majorityElement1(nums);
-        Set<Integer> setIds = new HashSet<>();
-        while (setIds.size() < 30) {
-            setIds.add((int) (Math.random() * nums.length));
-        }
-        int[] arr = new int[setIds.size()];
-        int count = 0;
-        for (int i : setIds) arr[count++] = nums[i];
-        return majorityElement1(arr);
-    }
+    public List<Integer> majorityElement(int[] arr) {
+        int n = arr.length; //size of the array
+        List<Integer> result = new ArrayList<>(); // list of answers
 
-    private List<Integer> majorityElement1(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            if (map.containsKey(n)) map.put(n, map.get(n) + 1);
-            else map.put(n, 1);
+        for (int i = 0; i < n; i++) {
+            //selected element is arr[i]:
+            // Checking if arr[i] is not already
+            // a part of the answer:
+            if (result.size() == 0 || result.get(0) != arr[i]) {
+                int count = 0;
+                for (int j = 0; j < n; j++) {
+                    // counting the frequency of arr[i]
+                    if (arr[j] == arr[i]) {
+                        count++;
+                    }
+                }
+
+                // check if frequency is greater than n/3:
+                if (count > (n / 3))
+                    result.add(arr[i]);
+            }
+
+            if (result.size() == 2) break;
         }
-        int indicator = nums.length / 3;
-        List<Integer> list = new ArrayList<>();
-        for (int key : map.keySet())
-            if (map.get(key) > indicator) list.add(key);
-        return list;
+
+        return result;
     }
 }
